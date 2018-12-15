4d6e8a65190805d83f8457af34cb4308182af933
Sander <sander@plex.tv>
2018-12-15 18:18:32 +0100

    Rewrite and update; no longer depends on URL Service in Services.bundle

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,8 +1,17 @@
+import ssl, urllib2
+
+PREFIX = '/video/cbs'
 ART = 'art-default.jpg'
 ICON = 'icon-default.jpg'
 
+HTTP_HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.2 Safari/605.1.15'
+}
+
 SHOWS_URL = 'https://www.cbs.com/shows/{}'
-SECTION_CAROUSEL = 'https://www.cbs.com/carousels/videosBySection/{}/offset/0/limit/40/xs/0'
+EPISODES_JSON_URL = 'https://www.cbs.com/shows/{}/video/xhr/episodes/page/0/size/50/xs/0/season/'
+TP_VIDEO_URL = 'http://link.theplatform.com/s/dJ5BDC/media/guid/2198311517/{}?mbr=true&assetTypes=StreamPack&formats=MPEG4,M3U'
+
 CATEGORIES = [
     {'category_id': 'originals', 'title': 'Originals'},
     {'category_id': 'drama', 'title': 'Drama'},
@@ -15,19 +24,18 @@ CATEGORIES = [
     {'category_id': 'news', 'title': 'News'}
 ]
 
-RE_SECTION_IDS = Regex('(?:video\.section_ids = |"section_ids"\:)\[([^\]]+)\]')
-RE_SECTION_METADATA = Regex('(?:video\.section_metadata = |"section_metadata"\:)({.+?}})')
-RE_SEASONS = Regex('video\.seasons = (.+?);', Regex.DOTALL)
-
 EXCLUDE_SHOWS = []
-
-PREFIX = '/video/cbs'
 
 ####################################################################################################
 def Start():
 
+    ObjectContainer.title1 = 'CBS'
+    DirectoryObject.thumb = R(ICON)
+
+    Dict['episodes'] = {}
+
     try:
-        json_obj = JSON.ObjectFromURL('http://ip-api.com/json', cacheTime=10)
+        json_obj = JSON.ObjectFromURL('http://ip-api.com/json', headers=HTTP_HEADERS, cacheTime=10)
     except:
         Log("IP Address Check Failed")
         json_obj = None
@@ -37,10 +45,6 @@ def Start():
         Log("  According to your IP address you are not in the United States.")
         Log("  Due to geo-blocking by the content provider, this channel does not work outside the United States.")
         Log("====================================================================================================")
-
-    ObjectContainer.title1 = 'CBS'
-    HTTP.CacheTime = CACHE_1HOUR
-    HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36'
 
 ####################################################################################################
 @handler(PREFIX, 'CBS', thumb=ICON, art=ART)
@@ -62,7 +66,7 @@ def MainMenu():
 def Shows(cat_title, cat_id):
 
     oc = ObjectContainer(title2=cat_title)
-    html = HTML.ElementFromURL(SHOWS_URL.format(cat_id))
+    html = HTML.ElementFromString(GetData(SHOWS_URL.format(cat_id)))
 
     for item in html.xpath('//article[@class="show-browse-item"]//img'):
 
@@ -71,18 +75,11 @@ def Shows(cat_title, cat_id):
         if title.lower() in EXCLUDE_SHOWS or 'previews' in title.lower() or 'premieres' in title.lower():
             continue
 
-        url = item.xpath('./parent::div/parent::a/@href')[0]
-
-        if not url.startswith('http'):
-            url = 'https://www.cbs.com/{}'.format(url.lstrip('/'))
-
-        if not url.endswith('/video/'):
-            url = '{}/video/'.format(url.rstrip('/'))
-
+        slug = item.xpath('./parent::div/parent::a/@href')[0].rstrip('/').split('/')[-1]
         thumb = item.get('data-src')
 
         oc.add(DirectoryObject(
-            key = Callback(Category, title=title, url=url, thumb=thumb),
+            key = Callback(Episodes, title=title, slug=slug),
             title = title,
             thumb = thumb
         ))
@@ -90,193 +87,105 @@ def Shows(cat_title, cat_id):
     return oc
 
 ####################################################################################################
-@route(PREFIX + '/category')
-def Category(title, url, thumb):
+@route(PREFIX + '/episodes')
+def Episodes(title, slug):
 
     oc = ObjectContainer(title2=unicode(title))
+    json_obj = JSON.ObjectFromString(GetData(EPISODES_JSON_URL.format(slug)))
 
-    try:
-        content = HTTP.Request(url).content
-    except:
-        return ObjectContainer(header="Empty", message="Can't find videos for this show.")
+    if 'result' in json_obj and 'data' in json_obj['result']:
 
-    try:
-        carousel_list = RE_SECTION_IDS.search(content).group(1).split(',')
-        carousel_metalist = RE_SECTION_METADATA.search(content).group(1)
-    except:
-        carousel_list = []
+        for video in json_obj['result']['data']:
 
-    for carousel in carousel_list:
-
-        json_obj = JSON.ObjectFromString(carousel_metalist)[carousel]
-
-        if 'all access' in json_obj['title'].lower() or json_obj['title'].lower() == 'free full episodes':
-            continue
-
-        json_url = SECTION_CAROUSEL.format(carousel)
-
-        # If there are seasons displayed then the json URL for each season must be pulled
-        try:
-            display_seasons = json_obj['display_seasons']
-        except:
-            display_seasons = False
-
-        if display_seasons:
-
-            season_list = RE_SEASONS.search(content)
-
-            if not season_list:
+            if video['type'] != "Full Episode" or video['status'] != "AVAILABLE":
                 continue
 
-            season_json = JSON.ObjectFromString(season_list.group(1))
+            content_id = video['content_id']
 
-            for item in season_json['filter']:
+            if content_id not in Dict['episodes']:
 
-                # Check if there are seasons that have free content
-                if item['total_count'] == item['premiumCount']:
-                    continue
-                else:
-                    title = json_obj['title']
+                Dict['episodes'][content_id] = {
+                    'show': video['series_title'],
+                    'title': video['episode_title'],
+                    'originally_available_at': Datetime.ParseDate(video['airdate']).date(),
+                    'season': video['season_number'],
+                    'index': video['episode_number'],
+                    'duration': video['duration_raw'] * 1000,
+                    'thumb': video['thumb']['large']
+                }
 
-                    oc.add(DirectoryObject(
-                        key = Callback(Seasons, title=title, thumb=thumb, json_url=json_url, season_list=season_list.group(1)),
-                        title = title,
-                        thumb = thumb
-                    ))
-
-                    break
-        else:
-
-            json_obj = JSON.ObjectFromURL(json_url)
-
-            if json_obj['success']:
-
-                # Check for at least 1 available video before adding this section
-                for video in json_obj['result']['data']:
-
-                    if 'status' in video and video['status'].lower() == 'available':
-
-                        title = json_obj['result']['title']
-
-                        oc.add(DirectoryObject(
-                            key = Callback(Video, title=title, json_url=json_url),
-                            title = title,
-                            thumb = thumb
-                        ))
-
-                        break
-
-                    else:
-                        continue
+            oc.add(CreateEpisodeObject(content_id=content_id))
 
     if len(oc) < 1:
-        Log("There are no video sections to list right now.")
-        return ObjectContainer(header="Empty", message="There are no video sections to list right now.")
+        Log("There aren't any episodes available for this show.")
+        return ObjectContainer(header="None Available", message="There aren't any episodes available for this show.")
     else:
         return oc
 
 ####################################################################################################
-# This function pulls the season numbers for any season that contains free content to add to the json url
-@route(PREFIX + '/seasons')
-def Seasons(title, thumb, json_url, season_list):
+@route(PREFIX + '/create_episodeobject', include_container=bool)
+def CreateEpisodeObject(content_id, include_container=False, **kwargs):
 
-    oc = ObjectContainer(title2=title)
+    episode = Dict['episodes'][content_id]
+
+    episode_obj = EpisodeObject(
+        key = Callback(CreateEpisodeObject, content_id=content_id, include_container=True),
+        rating_key = "cbs:{}".format(content_id),
+        show = episode['show'],
+        title = "{}x{}: {}".format(episode['season'], episode['index'], episode['title']) if episode['season'] and episode['index'] else episode['title'],
+        originally_available_at = episode['originally_available_at'],
+        season = int(episode['season']) if episode['season'] else None,
+        index = int(episode['index']) if episode['index'] else None,
+        duration = episode['duration'],
+        thumb = episode['thumb'],
+        items = [
+            MediaObject(
+                parts = [
+                    PartObject(key=HTTPLiveStreamURL(Callback(PlayVideo, content_id=content_id)))
+                ],
+                video_resolution = '1080',
+                audio_channels = 2,
+                optimized_for_streaming = True
+            )
+        ]
+    )
+
+    if include_container:
+        return ObjectContainer(objects=[episode_obj])
+    else:
+        return episode_obj
+
+####################################################################################################
+@route(PREFIX + '/playvideo.m3u8')
+@indirect
+def PlayVideo(content_id, **kwargs):
 
     try:
-        season_json = JSON.ObjectFromString(season_list)
+        tp = HTTP.Request(TP_VIDEO_URL.format(content_id), headers=HTTP_HEADERS, follow_redirects=False).content
+    except Ex.RedirectError, e:
+        if 'Location' in e.headers:
+            if not '?' in e.headers['Location']:
+                video_url = "{}?__b__=5000".format(e.headers['Location'])
+            else:
+                video_url = "{}&__b__=5000".format(e.headers['Location'])
+
+            return IndirectResponse(VideoClipObject, key=HTTPLiveStreamURL(url=video_url))
+        else:
+            Log("No redirect URL found")
+            raise Ex.MediaNotAvailable
     except:
-        return ObjectContainer(header="Empty", message="Can't find videos for this season.")
-
-    for item in season_json['filter']:
-
-        # Skip seasons that only offer premium content
-        if item['total_count'] == item['premiumCount']:
-            continue
-
-        title = item['title']
-        season_url = '{}/{}/'.format(json_url, item['season'])
-
-        json_obj = JSON.ObjectFromURL(season_url)
-
-        if json_obj['success']:
-
-            oc.add(DirectoryObject(
-                key = Callback(Video, title=title, json_url=season_url),
-                title = title,
-                thumb = thumb
-            ))
-
-    if len(oc) < 1:
-        Log("There are no seasons to list right now.")
-        return ObjectContainer(header="Empty", message="There are no seasons to list right now.")
-    else:
-        return oc
+        Log("HTTP request for TP failed")
+        raise Ex.MediaNotAvailable
 
 ####################################################################################################
-@route(PREFIX + '/video')
-def Video(title, json_url):
+@route(PREFIX + '/getdata')
+def GetData(url):
 
-    oc = ObjectContainer(title2=title)
+    # Quick and dirty workaround
+    # Do not validate ssl certificate
+    # http://stackoverflow.com/questions/27835619/ssl-certificate-verify-failed-error
+    req = urllib2.Request(url, headers=HTTP_HEADERS)
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+    data = urllib2.urlopen(req, context=ssl_context).read()
 
-    for video in JSON.ObjectFromURL(json_url)['result']['data']:
-
-        if not 'status' in video or video['status'].lower() != 'available':
-            continue
-
-        title = video['title'].split(' - ', 1)[-1]
-        vid_type = video['type']
-
-        thumb = video['thumb']['large']
-        if not thumb:
-            thumb = video['thumb']['small']
-
-        airdate = Datetime.ParseDate(video['airdate']).date()
-
-        url = video['url']
-        if not url.startswith('http'):
-            url = 'https://www.cbs.com/{}'.format(url.lstrip('/'))
-
-        if vid_type == 'Clip':
-
-            oc.add(VideoClipObject(
-                url = url,
-                title = title,
-                originally_available_at = airdate,
-                thumb = thumb
-            ))
-
-        elif vid_type == 'Full Episode':
-
-            show = video['series_title']
-
-            (season, episode, duration) = (video['season_number'], video['episode_number'], video['duration'])
-            season = int(season) if season is not None and season != '' else None
-
-            # Found an episode value that had two numbers separated by a comma so use try/except instead
-            try:
-                index = int(episode)
-            except:
-                index = None
-
-            duration = Datetime.MillisecondsFromString(duration) if duration is not None else None
-            summary = video['description'] if 'description' in video else None
-
-            oc.add(EpisodeObject(
-                url = url,
-                show = show,
-                title = "S{}E{} - {}".format(str(season).zfill(2), str(index).zfill(2), title) if season and index else title,
-                summary = summary,
-                originally_available_at = airdate,
-                season = season,
-                index = index,
-                duration = duration,
-                thumb = thumb
-            ))
-
-    oc.objects.sort(key=lambda obj: obj.originally_available_at, reverse=True)
-
-    if len(oc) < 1:
-        Log("There are no videos to list right now.")
-        return ObjectContainer(header="Empty", message="There are no videos to list right now.")
-    else:
-        return oc
+    return data
